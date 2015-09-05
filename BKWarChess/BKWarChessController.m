41e86029acd36fd98cd84f3fd69a5e3ceea75d31
锄禾日当午 <1178693380@qq.com>
2015-09-05 14:05:04 +0800

    new

--- BKWarChess/BKWarChessController.m
+++ BKWarChess/BKWarChessController.m
@@ -11,7 +11,11 @@
 #import "BKPersonBtn.h"
 
 #define ContentDistance 10
-@interface BKWarChessController ()
+#define MoveDistance self.map.width / 4
+
+@interface BKWarChessController (){
+    int mapScore[5][5];
+}
 
 @property (weak, nonatomic) UIImageView *map;
 
@@ -84,19 +88,36 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    
+    [self initMapScore];
+    
     self.view.backgroundColor = [UIColor blueColor];
     [self setUpUI];
+}
+
+-(void)initMapScore
+{
+    for(int i = 0; i < 5; i++){
+        for(int j = 0; j< 5; j++){
+            mapScore[i][j] = 0;
+        }
+    }
 }
 
 -(void)setUpUI
 {
     [self map];
-    [self personBtnA];
-    [self personBtnB];
     UIImageView *bg = [[UIImageView alloc] initWithFrame:self.view.bounds];
     bg.image = [UIImage imageNamed:@"bg"];
     bg.userInteractionEnabled = YES;
     [self.view insertSubview:bg atIndex:0];
+    
+    UIButton *topBtn = [[UIButton alloc] init];
+    topBtn.frame = CGRectMake(ContentDistance, ContentDistance * 3, self.view.width - 2 * ContentDistance, 50);
+    topBtn.userInteractionEnabled = NO;
+    [topBtn setBackgroundImage:[UIImage imageNamed:@"kuang"] forState:UIControlStateNormal];
+    [topBtn setTitle:@"火箭队出击" forState:UIControlStateNormal];
+    [self.view addSubview:topBtn];
     
     UIButton *portraitBtnA = [[UIButton alloc] init];
     portraitBtnA.frame = CGRectMake(ContentDistance, CGRectGetMaxY(self.map.frame) + ContentDistance * 3, 80, 80);
@@ -117,7 +138,74 @@
     vsBtn.userInteractionEnabled = NO;
     [self.view addSubview:vsBtn];
     
-
+    UILabel *portraitLabelA = [[UILabel alloc] init];
+    portraitLabelA.x = CGRectGetMinX(portraitBtnA.frame);
+    portraitLabelA.y = CGRectGetMaxY(portraitBtnA.frame) + ContentDistance;
+    portraitLabelA.width = portraitBtnA.width;
+    portraitLabelA.height = 40;
+    portraitLabelA.text = @"火箭队";
+    portraitLabelA.textAlignment = NSTextAlignmentCenter;
+    portraitLabelA.textColor = [UIColor whiteColor];
+    [self.view addSubview:portraitLabelA];
+    
+    UILabel *portraitLabelB = [[UILabel alloc] init];
+    portraitLabelB.x = CGRectGetMinX(portraitBtnB.frame);
+    portraitLabelB.y = CGRectGetMaxY(portraitBtnB.frame) + ContentDistance;
+    portraitLabelB.width = portraitBtnB.width;
+    portraitLabelB.height = 40;
+    portraitLabelB.text = @"岩浆队";
+    portraitLabelB.textAlignment = NSTextAlignmentCenter;
+    portraitLabelB.textColor = [UIColor whiteColor];
+    [self.view addSubview:portraitLabelB];
+    
+    //map上元素
+    BKPersonBtn *megaBtnA = [BKPersonBtn getBKPersonBtn];
+    megaBtnA.center = CGPointMake(CGRectGetMaxX(self.map.frame), CGRectGetMinY(self.map.frame));
+    [megaBtnA setBackgroundImage:[UIImage imageNamed:@"mega"] forState:UIControlStateNormal];
+    megaBtnA.userInteractionEnabled = NO;
+    [self.view addSubview:megaBtnA];
+    
+    BKPersonBtn *megaBtnB = [BKPersonBtn getBKPersonBtn];
+    megaBtnB.center = CGPointMake(CGRectGetMaxX(self.map.frame) - 2 * MoveDistance, CGRectGetMinY(self.map.frame) + 2 * MoveDistance);
+    [megaBtnB setBackgroundImage:[UIImage imageNamed:@"mega"] forState:UIControlStateNormal];
+    megaBtnB.userInteractionEnabled = NO;
+    [self.view addSubview:megaBtnB];
+    
+    BKPersonBtn *megaBtnC = [BKPersonBtn getBKPersonBtn];
+    megaBtnC.center = CGPointMake(CGRectGetMinX(self.map.frame), CGRectGetMaxY(self.map.frame));
+    [megaBtnC setBackgroundImage:[UIImage imageNamed:@"mega"] forState:UIControlStateNormal];
+    megaBtnC.userInteractionEnabled = NO;
+    [self.view addSubview:megaBtnC];
+    
+    BKPersonBtn *spriteA = [BKPersonBtn getBKPersonBtn];
+    spriteA.center = CGPointMake(CGRectGetMinX(self.map.frame) + MoveDistance, CGRectGetMinY(self.map.frame) + MoveDistance);
+    [spriteA setBackgroundImage:[UIImage imageNamed:@"dragon"] forState:UIControlStateNormal];
+    spriteA.width = 50;
+    spriteA.height = 50;
+    spriteA.userInteractionEnabled = NO;
+    [spriteA setTitle:@"Lv.1" forState:UIControlStateNormal];
+    [self.view addSubview:spriteA];
+    
+    BKPersonBtn *spriteB = [BKPersonBtn getBKPersonBtn];
+    spriteB.center = CGPointMake(CGRectGetMaxX(self.map.frame) - MoveDistance, CGRectGetMaxY(self.map.frame) -  MoveDistance);
+    [spriteB setBackgroundImage:[UIImage imageNamed:@"bikaqiu"] forState:UIControlStateNormal];
+    spriteB.width = 50;
+    spriteB.height = 50;
+    spriteB.userInteractionEnabled = NO;
+    [spriteB setTitle:@"Lv.1" forState:UIControlStateNormal];
+    [self.view addSubview:spriteB];
+    
+    BKPersonBtn *spriteC = [BKPersonBtn getBKPersonBtn];
+    spriteC.center = CGPointMake(CGRectGetMinX(self.map.frame) + MoveDistance, CGRectGetMaxY(self.map.frame) -  MoveDistance);
+    [spriteC setBackgroundImage:[UIImage imageNamed:@"flag"] forState:UIControlStateNormal];
+    spriteC.width = 50;
+    spriteC.height = 50;
+    spriteC.userInteractionEnabled = NO;
+    [spriteC setTitle:@"Lv.2" forState:UIControlStateNormal];
+    [self.view addSubview:spriteC];
+    
+    [self personBtnA];
+    [self personBtnB];
 }
 
 - (void)didReceiveMemoryWarning {
